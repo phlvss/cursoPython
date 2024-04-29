1f6879c2db7c0d65c42a40e7e6f69a4fc226d0de
phlvss <paulolvs6@gmail.com>
2024-04-29 15:40:24 -0300

    continuacão das aulas

--- Aulas/Estruturas de Dados/04 - Dicionários/02_dicionarios_aninhados.py
+++ Aulas/Estruturas de Dados/04 - Dicionários/02_dicionarios_aninhados.py
@@ -2,8 +2,11 @@ contatos = {
     "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
     "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
     "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
-    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766", "extra": {"idade": 18}}
 }
 
 telefone = contatos["giovanna@gmail.com"]["telefone"]  # "3443-2121"
+print(telefone)
+
+telefone = contatos["melaine@gmail.com"]["extra"]["idade"]
 print(telefone)
